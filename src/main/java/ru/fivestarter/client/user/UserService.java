22c853f935610c13172c60d5c546e5e7c4386a9f
fivestarter <fivestarter@gmail.com>
2020-05-31 22:12:09 +0300

    webclient was added

--- src/main/java/ru/fivestarter/client/user/UserService.java
+++ src/main/java/ru/fivestarter/client/user/UserService.java
@@ -1,13 +1,22 @@
 package ru.fivestarter.client.user;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class UserService {
+
+    private final WebClient webClient;
+
+    public UserService() {
+        this.webClient = WebClient.builder().baseUrl("http://localhost:9090/api/v1/users")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
     public UserJson getUser() {
-        UserJson user = new UserJson();
-        user.setFirstName("firstName");
-        user.setLastName("lastName");
-        return user;
+        return webClient.get().uri("/").retrieve().bodyToMono(UserJson.class).block();
     }
 }
